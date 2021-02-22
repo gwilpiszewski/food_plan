d19156a6f20034fe7165d0ca35e5ba2f612fc259
gwilpiszewski <ZnDn6zJv9sxGBDc>
2021-02-22 15:22:06 +0100

    1.2_Model_przepisu

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -2,3 +2,13 @@ from django.db import models
 
 
 # Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=128)
+    ingredients = models.CharField(max_lenght=500)
+    description = models.CharField(max_length=1200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    preparation_time = models.DurationField()
+    votes = models.SmallIntegerField()
+

f4ba396d765406f35c41ece18679062df020050e
bartosz98765 <wiem@poczta.fm>
2021-02-22 21:47:12 +0100

    Testowy

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -4,17 +4,40 @@ from django.shortcuts import render
 from django.views import View
 from jedzonko.models import Recipe
 
+
 class IndexView(View):
 
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
         return render(request, "index.html", ctx)
 
+
 class RecipeListView(View):
 
     def get(self, request):
-        context = {}
-        return render(request, 'app-recipes.html', {})       
+        # template = f"""
+        #             <tr class="d-flex">
+        #             <th scope="row" class="col-1">1</th>
+        #             <td class="col-2">
+        #                 {{ recipes_title }}
+        #             </td>
+        #             <td class="col-7"> {{ recipes_content}}
+        #             </td>
+        #             <td class="col-2 d-flex align-items-center justify-content-center flex-wrap">
+        #                 <a href="#"
+        #                    class="btn btn-danger rounded-0 text-light m-1">Usuń</a>
+        #                 <a href="/app-recipe-details.html"
+        #                    class="btn btn-info rounded-0 text-light m-1">Szczegóły</a>
+        #                 <a href="/app-edit-recipe.html"
+        #                    class="btn btn-warning rounded-0 text-light m-1">Edytuj</a>
+        #             </td>
+        #             </tr>
+        # """
+
+        myrange = [1, 2, 3, 4, 5, 6]
+        ctx = {'myrange': myrange}
+        # ctx = {'template': template}
+        return render(request, 'app-recipes.html', ctx)
 
 
 class DashboardView(View):

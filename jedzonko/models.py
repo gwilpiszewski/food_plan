46442064b81acadff9cc18527d174661956fb4fd
bartosz98765 <wiem@poczta.fm>
2021-02-23 11:02:24 +0100

    5.1 Zapisanie zmian

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -12,14 +12,14 @@ class Recipe(models.Model):
     preparation_time = models.SmallIntegerField(default=0)
     votes = models.SmallIntegerField(default=0)
 
-class Plan(models.Model):
-    name = models.CharField(max_length=128)
-    description = models.TextField(max_length=1200)
-    created = models.DateTimeField(auto_now_add=True)
-    recipes = models.ManyToManyField(Recipe, through="RecipePlan")
-
-class RecipePlan(models.Model):
-    meal_name = models.CharField(max_length=32)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
-    order = models.
+# class Plan(models.Model):
+#     name = models.CharField(max_length=128)
+#     description = models.TextField(max_length=1200)
+#     created = models.DateTimeField(auto_now_add=True)
+#     recipes = models.ManyToManyField(Recipe, through="RecipePlan")
+#
+# class RecipePlan(models.Model):
+#     meal_name = models.CharField(max_length=32)
+#     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+#     plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
+#     order = models.
